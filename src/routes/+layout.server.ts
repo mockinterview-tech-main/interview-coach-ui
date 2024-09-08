75f0c8b04953d708ee3bb3339348d20b5db7740f
Dominick Caponi <d.caponi1@gmail.com>
2024-09-08 15:24:32 -0700

    login loop debug

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -9,9 +9,9 @@ const stripe = new Stripe(import.meta.env['VITE_STRIPE_SECRET_KEY'], {
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
     const protectedRoutes = ['interview', 'summary', 'credits']
+            console.log("LOGIN LOOP", locals.pb?.authStore.isValid, url.pathname.split("/").filter(Boolean)[0])
 
     if (!locals.pb?.authStore.isValid && protectedRoutes.includes(url.pathname.split("/").filter(Boolean)[0])) {
-        console.log("LOGIN LOOP", locals.pb?.authStore.isValid, url.pathname.split("/").filter(Boolean)[0])
         throw redirect(302, '/login')
     }
 
